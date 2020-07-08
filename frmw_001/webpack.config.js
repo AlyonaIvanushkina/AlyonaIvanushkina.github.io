9215d01257859f71b0a7726ed687a7c857aba6a3
AlyonaIvanushkina <alyona.ivanushkina@gmail.com>
2020-07-08 22:41:59 +0300

    build frmw_001

--- frmw_001/webpack.config.js
+++ frmw_001/webpack.config.js
@@ -1,7 +1,8 @@
 module.exports = {
     module: {
         rules: [
-            {
+            {   
+                test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader'
